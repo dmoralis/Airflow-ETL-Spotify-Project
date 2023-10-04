41fb30372e292c7f1566b5ef1e496fec0c6a8350
dmoralis <dmoralis@csd.auth.gr>
2023-10-04 20:14:38 +0300

    __init__.py files added in order to get directory properties. Also in spotify_methods.py 'request_spotify_refresh_token' method runs localy w/ the assist of __name__='__main__' in the first execute in order to request the authentication code and refresh token.

--- spotify_methods.py
+++ spotify_methods.py
@@ -10,10 +10,12 @@ import base64
 from urllib.parse import urlencode, urlparse, parse_qs
 
 
-CLIENT_ID = "**"
-CLIENT_SECRET = "**"
+CLIENT_ID = "**52e"
+CLIENT_SECRET = "**d9f"
 REDIRECT_URI = "http://localhost:8888/callback"
 DATABASE_LOCATION = "postgresql://airflow:airflow@a39e18f45b63:5432/airflowspotify-postgres-1"
+
+
 def check_if_valid_data(df: pd.DataFrame) -> bool:
     # Check if dataframe is empty
     if df.empty:
@@ -41,11 +43,13 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
 
     return True
 
+
 def request_token():
     # Check if a file containing the refresh token exists
     if open('refresh_token.txt', "r"):
         f = open('refresh_token.txt', "r")
         refresh_token = f.readline()
+        print(f'Regresh token read from file {refresh_token}')
     else:
         print('No file refresh_token.txt found')
         return -1
@@ -57,37 +61,40 @@ def request_token():
         'refresh_token': refresh_token
     }
     headers = {
-        'Authorization': f'Basic {CLIENT_ID}:{CLIENT_SECRET}'
+        'Authorization': 'Basic ' + base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
     }
-    response = requests.get(token_url, data=data, headers=headers).json()
-    if response.status_code == 200:
+    response = requests.post(token_url, headers=headers, data=data).json()
+    if response:
         token = response.get('access_token')
     else:
-        print(f'Token could not be taken due to error code: {response.status_code}')
+        print(f'Token could not be taken due to error code: {response.get("error_code")}')
         return -1
 
     # Using the token now request the wanted data
-    response = requests.get('https://api.spotify.com/v1/me/player/recently-played', header={'Authorization':
+    response = requests.get('https://api.spotify.com/v1/me/player/recently-played', headers={'Authorization':
                                                                                                 f'Bearer {token}'})
-    return response
+    return response.json()
+
 
 def request_spotify_refresh_token():
     print(f'This methods require from the user to open a link and paste it in the runtime terminal, in order to '
           f'proceed to the request of the refresh token')
 
     auth_url = 'https://accounts.spotify.com/authorize?'
-    scope = 'user-read-private'
+    scope = 'user-read-private user-read-email user-modify-playback-state user-read-playback-position' \
+            ' user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private'
     data = {
-        'response_type': 'code',
         'client_id': CLIENT_ID,
+        'response_type': 'code',
         'scope': scope,
         'redirect_uri': REDIRECT_URI}
-    auth_url = auth_url + '?' + urlencode(query=data)
+    auth_url = f"{auth_url}{urlencode(data)}"
 
     print(f'You need to open this {auth_url} url in your browser, while you are logged on your spotify account, and get the url '
           f'result')
     auth_answer = input("Insert the link that resulted in the URL:")
-    auth_code = parse_qs((urlparse(auth_answer)).query)['data'][0]
+    auth_code = parse_qs((urlparse(auth_answer)).query)['code'][0]
+    print(f'Auth code: {auth_code}')
 
     token_url = 'https://accounts.spotify.com/api/token'
     data = {
@@ -96,20 +103,24 @@ def request_spotify_refresh_token():
         'grant_type': 'authorization_code'
     }
     headers = {
-        'Authorization': 'Basic' + f'{CLIENT_ID}:{CLIENT_SECRET}'
+        'Authorization': 'Basic ' + base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
     }
 
-    response = requests.get(token_url, data=data, headers=headers).json()
+    response = requests.post(token_url, data=data, headers=headers).json()
 
-    if response.status_code == 200:
+    if response:
         print('Successful request of the token..\n Proceeds in saving the refresh token in a refresh_token.txt file')
+        print(f'Response {response}')
         with open('refresh_token.txt', "w+") as refresh_token_file:
             refresh_token_file.write(response.get('refresh_token'))
         print('Save is done\nTrying out if the refresh token works')
-
+        response_test = request_token()
+        if response_test:
+            print('The new refresh token works')
+        else:
+            print(f'The new refresh token doesn\'t work with error code: {response}')
     else:
-        print(f'The token request failed with status code:{response.status_code}')
-
+        print(f"The token request failed with status code:{response.get('status_code')}")
 
 
 def spotify_etl():
@@ -173,3 +184,9 @@ def spotify_etl():
         print("Data already exists in the database")
 
     conn.close()
+
+
+if __name__ == '__main__':
+    #request_spotify_refresh_token()
+    #print(request_token())
+    print('dummy print')
