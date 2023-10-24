85383541f805dd48315b8c3f9fdc16cb2619b273
dmoralis <dmoralis@csd.auth.gr>
2023-10-24 22:11:24 +0300

    Another DAG added in order to transform and save the tables artists, streaming, played_tracks to the PostgreSQL DB

--- dags/spotify_dag.py
+++ dags/spotify_dag.py
@@ -6,7 +6,7 @@ try:
     sys.path.insert(0, parent_dir)
     from datetime import timedelta, datetime
     from airflow import DAG
-    from spotify_methods import spotify_etl
+    from spotify_methods import fetch_data_and_create_tables, transform_and_load_data
     from airflow.operators.python_operator import PythonOperator
     import pandas as pd
     print("All Dag modules are ok ......")
@@ -21,10 +21,18 @@ with DAG(
             "owner": "airflow",
             "retries": 1,
             "retry_delay": timedelta(minutes=5),
-            "start_date": datetime(2023, 3, 10)
+            "start_date": datetime(2023, 10, 23)
         }) as f:
-    run_etl = PythonOperator(
-        task_id="ETL_Spotify",
-        python_callable=spotify_etl
+    run_fetch = PythonOperator(
+        task_id="Fetch_and_create_tables",
+        provide_context=True,
+        python_callable=fetch_data_and_create_tables
     )
 
+    transform_and_save = PythonOperator(
+        task_id="transform_and_load_data",
+        provide_context=True,
+        python_callable=transform_and_load_data
+    )
+
+    run_fetch >> transform_and_save
--- dags/spotify_methods.py
+++ dags/spotify_methods.py
@@ -2,6 +2,7 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
 import requests
+from collections import defaultdict
 import json
 from datetime import datetime
 import datetime
@@ -18,20 +19,23 @@ REFRESH_TOKEN = "AQ**"
 DATABASE_LOCATION = "postgresql://airflow:airflow@a39e18f45b63:5432/airflow"
 
 #connection_string = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-def check_if_valid_data(df: pd.DataFrame) -> bool:
+def check_if_valid_data(df: pd.DataFrame, ) -> bool:
     # Check if dataframe is empty
     if df.empty:
         print("No songs downloaded. Finishing execution")
         return False
 
     # Primary Key Check
-    if pd.Series(df['played_at']).is_unique:
-        pass
-    else:
-        raise Exception("Primary Key check is violated")
+    #if pd.Series(df['played_at']).is_unique:
+    #    pass
+    #else:
+    #    raise Exception("Primary Key check is violated")
 
     # Check for nulls
     if df.isnull().values.any():
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        print(df)
         raise Exception("Null values found")
 
     '''# Check that all timestamps are of yesterday's date
@@ -46,18 +50,9 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
     return True
 
 
-def request_token_without_authentication(refresh_token=None):
+def request_token_without_authentication(refresh_token=None, api_link='https://api.spotify.com/v1/me/'
+                                                                        'player/recently-played'):
     # Check if a file containing the refresh token exists
-    '''if os.path.isfile('../refresh_token.txt'):
-        f = open('../refresh_token.txt', "r")
-        refresh_token = f.readline()
-        print(f'Refresh token read from file {refresh_token}')
-    else:
-        print('No file refresh_token.txt found, please run \'request_spotify_refresh_token\' first.')
-        print(f"Current dir {os.listdir('.')} \n Previous dir {os.listdir('..')}")
-        response = requests.models.Response()
-        response.status_code = -1
-        return response'''
     if refresh_token is None:
         refresh_token = REFRESH_TOKEN
 
@@ -78,12 +73,11 @@ def request_token_without_authentication(refresh_token=None):
         return response
 
     # Using the token now request the wanted data
-    response = requests.get('https://api.spotify.com/v1/me/player/recently-played', headers={'Authorization':
-                                                                                                f'Bearer {token}'})
+    response = requests.get(api_link, headers={'Authorization': f'Bearer {token}'})
     return response
 
 
-def request_token_with_authentication():
+def request_refresh_token_with_authentication():
     print(f'This methods require from the user to open a link and paste it in the runtime terminal, in order to '
           f'proceed to the request of the refresh token')
 
@@ -101,7 +95,6 @@ def request_token_with_authentication():
           f' and get the url result')
     auth_answer = input("Insert the link that resulted in the URL:")
     auth_code = parse_qs((urlparse(auth_answer)).query)['code'][0]
-    #print(f'Auth code: {auth_code}')
 
     token_url = 'https://accounts.spotify.com/api/token'
     data = {
@@ -115,13 +108,9 @@ def request_token_with_authentication():
 
     respo = requests.post(token_url, data=data, headers=headers)
     if respo.status_code == 200:
-        #print('Successful request of the token..\nProceeds in saving the refresh token in a refresh_token.txt file!')
         respo = respo.json()
         refresh_token = respo.get('refresh_token')
         print(f'Successful request of the token..\nPlease insert it in the constant variables and run the Airflow DAG \n{refresh_token}')
-        #with open('../refresh_token.txt', "w+") as refresh_token_file:
-        #    refresh_token_file.write(respo.get('refresh_token'))
-        #print('Save is done\nTrying out if the refresh token works')
         print('Trying out if the refresh token works')
         response_test = request_token_without_authentication(refresh_token=refresh_token)
         if response_test.status_code == 200:
@@ -133,7 +122,9 @@ def request_token_with_authentication():
         print(f"The token request failed with status code:{respo.status_code}")
 
 
-def spotify_etl():
+def fetch_data_and_create_tables(**kwargs):
+    ti = kwargs['ti']
+
     # Request data from Spotify API
     print('ZERO STAGE')
     data = request_token_without_authentication()
@@ -142,51 +133,94 @@ def spotify_etl():
         return -1
 
     data = data.json()
-    song_names = []
-    artist_names = []
-    played_at_list = []
-    timestamps = []
+
+    song_dict = defaultdict(list)
+    artist_dict = defaultdict(list)
+    streaming_dict = defaultdict(list)
+
     print('FIRST STAGE')
-    # Extracting only the relevant bits of data from the json object
+    # Create song dictionary w/ relevant infos
     try:
         for song in data["items"]:
-            song_names.append(song["track"]["name"])
-            artist_names.append(song["track"]["album"]["artists"][0]["name"])
-            played_at_list.append(song["played_at"])
-            timestamps.append(song["played_at"][0:10])
+            artist_id = song["track"]["artists"][0]["id"]
+
+            # Set listens info dictionary
+            streaming_dict["track_id"].append(song["track"]["id"])
+            streaming_dict["artist_id"].append(artist_id)
+            streaming_dict["played_timestamp"].append(song["played_at"])
+
+            # Set song info dictionary
+            song_dict['id'].append(song["track"]["id"])
+            song_dict['song_name'].append(song["track"]["name"])
+            song_dict['artist_name'].append(song["track"]["artists"][0]["name"])
+            song_dict['duration'].append(song["track"]["duration_ms"])
+            song_dict['is_explicit'].append(song["track"]["explicit"])
+            song_dict['is_in_album'].append(True if song["track"]["album"] == 'album' else False)
+
+            # Set artist info dictionary
+            if artist_id not in artist_dict["id"]:
+                artist_response = request_token_without_authentication(api_link=f"https://api.spotify.com/v1/artists/"
+                                                                                f"{artist_id}").json()
+                artist_dict['id'].append(artist_id)
+                artist_dict['followers'].append(artist_response["followers"]["total"])
+                artist_dict['genres'].append(artist_response["genres"])
+                artist_dict['popularity'].append(artist_response["popularity"])
+                artist_dict['name'].append(artist_response["name"])
+
     except KeyError as e:
-        print(f'Items not found because data is {data}')
-    # Prepare a dictionary in order to turn it into a pandas dataframe below
-    print('SECOND STAGE')
-    song_dict = {
-        "song_name": song_names,
-        "artist_name": artist_names,
-        "played_at": played_at_list,
-        "timestamp": timestamps
-    }
+        print(f'Items not found because data is {data} \n ERROR ON {e}')
+
     print(f'Song dict test print {song_dict}')
-    song_df = pd.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamp"])
+    song_df = pd.DataFrame(song_dict, columns=["id", "song_name", "artist_name", "duration",
+                                               "is_explicit", "is_in_album"])
+    artist_df = pd.DataFrame(artist_dict, columns=["id", "followers", "genres", "popularity", "name"])
+
+    streaming_df = pd.DataFrame(streaming_dict, columns=["track_id", "artist_id", "played_timestamp"])
 
     # Validate
     if check_if_valid_data(song_df):
-        print("Data valid, proceed to Load stage")
+        print("Songs Data valid, proceed to Load stage")
+
+    if check_if_valid_data(artist_df):
+        print("Artists Data valid, proceed to Load stage")
+
     print('THIRD STAGE')
     # Load
     engine = sqlalchemy.create_engine(DATABASE_LOCATION)
     Session = sessionmaker(bind=engine)
     session = Session()
     print('FOURTH STAGE')
-    sql_query = """
+    sql_query_create_tables = """
+        DROP TABLE IF EXISTS played_tracks;
+        DROP TABLE IF EXISTS streaming;
+        DROP TABLE IF EXISTS artists;
+        
         CREATE TABLE IF NOT EXISTS played_tracks(
-            id SERIAL PRIMARY KEY,
+            id VARCHAR(200) PRIMARY KEY,
             song_name VARCHAR(200),
             artist_name VARCHAR(200),
-            played_at VARCHAR(200),
-            timestamp VARCHAR(200)
-        )
+            duration INT,
+            is_explicit BOOLEAN,
+            is_in_album BOOLEAN
+        );
+        
+        CREATE TABLE IF NOT EXISTS streaming(
+            track_id VARCHAR(200),
+            artist_id  VARCHAR(200),
+            played_timestamp timestamp,
+            ingestion_date timestamp DEFAULT current_timestamp,
+            PRIMARY KEY (track_id, artist_id, played_timestamp)
+        );
+        
+        CREATE TABLE IF NOT EXISTS artists(
+                id VARCHAR(200) PRIMARY KEY,
+                followers INT,
+                genres text[],
+                popularity smallint,
+                name VARCHAR(200)
+        );
         """
-
-    session.execute(sql_query)
+    session.execute(sql_query_create_tables)
     print("Opened database successfully")
 
     # Use a raw SQL query to list databases
@@ -203,12 +237,59 @@ def spotify_etl():
     except:
         print("Data already exists in the database")
 
+    # pushes data in any_serializable_value into xcom with key "identifier as string"
+    ti.xcom_push(key="song_df", value=song_df)
+    ti.xcom_push(key="artist_df", value=artist_df)
+    ti.xcom_push(key="streaming_df", value=streaming_df)
+
     session.commit()
     session.close()
 
+def transform_and_load_data(**kwargs):
+    ti = kwargs['ti']
+
+    song_df = ti.xcom_pull(key="song_df")
+    artist_df = ti.xcom_pull(key="artist_df")
+    streaming_df = ti.xcom_pull(key="streaming_df")
+    print(f'Song df {song_df} \n\n artist df {artist_df} \n\n streaming df {streaming_df}')
+
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for index, row in song_df.iterrows():
+        session.execute(f"INSERT INTO played_tracks(id, song_name, artist_name, duration, is_explicit, is_in_album) "
+                        f"VALUES ('{row['id']}', '{row['song_name']}', '{row['artist_name']}', {row['duration']}, "
+                        f"{row['is_explicit']}, '{row['is_in_album']}') "
+                        f"ON CONFLICT (id) DO NOTHING")
+
+    for index, row in artist_df.iterrows():
+        session.execute(f"INSERT INTO artists(id, name, followers, genres, popularity) "
+                        f"VALUES('{row['id']}', '{row['name']}', {row['followers']}, ARRAY{row['genres']},"
+                        f" {row['popularity']}) ON CONFLICT (id) DO NOTHING")
+
+    for index, row in streaming_df.iterrows():
+        session.execute(f"INSERT INTO streaming(track_id, artist_id, played_timestamp) "
+                        f"VALUES('{row['track_id']}', '{row['artist_id']}', '{row['played_timestamp']}') "
+                        f"ON CONFLICT (track_id, artist_id, played_timestamp) DO NOTHING")
+
+
+
+    print('Data were loaded in the tables')
+    for temp in session.execute(f"SELECT * FROM artists;"):
+        print(temp)
+
+    print('QUERY2')
+    for temp in session.execute(f"SELECT * FROM played_tracks;"):
+        print(temp)
+
+    print('QUERY3')
+    for temp in session.execute(f"SELECT * FROM streaming;"):
+        print(temp)
 
 if __name__ == '__main__':
-    request_token_with_authentication()
+    print('dummy')
+    #request_refresh_token_with_authentication()
     #response = request_token()
     #print('dummy print')
     #spotify_etl()
